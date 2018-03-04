72639ea96e40a75ddb451d4b49d57b8dff012564
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-03-04 11:55:05 -0800

    started jumpsearch

--- sorts/JumpSearch.java
+++ sorts/JumpSearch.java
@@ -1,5 +1,5 @@
 package sorts;
-
+import java.util.ArrayList;
 public class JumpSearch {
  /**
   * A search that jumps k values, and checks if target value is in between those k values. If so, search for it. If not,
@@ -7,6 +7,13 @@ public class JumpSearch {
   *
   * **/
 
+    public int jumpSearch(ArrayList l, int jump, int target) {
+        int size = l.size();
+        int i = 0;
+        while (k < Math.sqrt(size) ) {
 
+        }
+        return 1;
+    }
 
 }
