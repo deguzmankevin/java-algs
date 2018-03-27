5c1d7c1d37eadc7afcc51c7654f990c50a34f115
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-03-26 23:31:52 -0700

    added stuff

---
+++ funstuff/Dragonball/FriezaSaga.java
@@ -0,0 +1,12 @@
+package funstuff.Dragonball;
+
+/**
+ * Created by Kevin on 3/26/2018.
+ */
+public class FriezaSaga {
+
+
+    public static void main (String[] args) {
+        Saiyan Goku = new Saiyan(50);
+    }
+}
